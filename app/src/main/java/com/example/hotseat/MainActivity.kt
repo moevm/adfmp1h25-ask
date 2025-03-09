d4d8b8040f1bf50b3af954169022d153fa726cd5
Дмитрий Кривоченко <dima.krivochenko@mail.ru>
2025-03-09 23:58:01 +0300

    refactored main screen

--- app/src/main/java/com/example/hotseat/MainActivity.kt
+++ app/src/main/java/com/example/hotseat/MainActivity.kt
@@ -7,10 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 import com.example.hotseat.ui.theme.HotseatTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HotseatTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Test",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "difficulty"
+                ) {
+                    composable("difficulty") {
+                        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                            DifficultySelectionScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                onBackClick = { /* Handle back button click */ },
+                                onDifficultySelected = { difficulty ->
+                                    navController.navigate("questionsList")
+                                }
+                            )
+                        }
+                    }
+                    composable("questionsList") {
+                        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                            QuestionsListScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                onBackClick = { navController.popBackStack() }
+                            )
+                        }
+                    }
+                    composable("ratings") {
+                        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                            RatingsScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                onBackClick = { navController.popBackStack() },
+                                onResetClick = { /* Handle reset */ }
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HotseatTheme {
-        Greeting("Android")
     }
 }
